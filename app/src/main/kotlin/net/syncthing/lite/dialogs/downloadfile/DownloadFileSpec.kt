6f238073517f090e1fe92989dbbe4235af84d8f8
Marius Volkhart <marius@volkhart.com>
2020-01-21 18:10:39 -0600

    Remove Android projects
    We don't intend to target Android for our efforts, so we're removing the Android projects to make development easier.

--- app/src/main/kotlin/net/syncthing/lite/dialogs/downloadfile/DownloadFileSpec.kt
+++
@@ -1,5 +0,0 @@
-package net.syncthing.lite.dialogs.downloadfile
-
-import java.io.Serializable
-
-data class DownloadFileSpec(val folder: String, val path: String, val fileName: String): Serializable
--- app/src/main/kotlin/net/syncthing/lite/library/DownloadFilePath.kt
+++
@@ -1,9 +0,0 @@
-package net.syncthing.lite.library
-
-import java.io.File
-
-data class DownloadFilePath (val baseDirectory: File, val fileHash: String) {
-    val filesDirectory = File(baseDirectory, fileHash.substring(0, 2))
-    val targetFile = File(filesDirectory, fileHash.substring(2))
-    val tempFile = File(filesDirectory, fileHash.substring(2) + "_temp")
-}
